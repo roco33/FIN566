c492a4eed84d06e276815b0fd92c4dd4011b9a87
roco33 <minhaogu@gmail.com>
2017-10-30 15:57:29 -0500

    sub

--- PS5/robot1_algo_aggressive_execution_PS5_part2_template.m
+++ PS5/robot1_algo_aggressive_execution_PS5_part2_template.m
@@ -14,7 +14,7 @@
 goal_inventory_level=1000;
 buy_sell_robot_j=1;%buy order 
 price_robot_j=max_price;
-FAK_indic=1;%1 for passive order; 0 for market order
+FAK_indic=0;%1 for passive order; 0 for market order
 
 
 %-------------------------------------------------%
@@ -23,9 +23,9 @@ FAK_indic=1;%1 for passive order; 0 for market order
 alive_indicator_robot_j=1;%alive order 
 
 %quantity 
-quantity_robot_j=1000/((t_max - burn_in_period)/g);
+% quantity_robot_j=1000/((t_max - burn_in_period)/g);
 % quantity_robot_j=min(1000/((t_max - burn_in_period)/g),depth_at_best_ask);
-% quantity_robot_j=depth_at_best_ask;
+quantity_robot_j=depth_at_best_ask;
 % if best_ask - best_bid > 1
 	% alive_indicator_robot_j=0;
 % end
